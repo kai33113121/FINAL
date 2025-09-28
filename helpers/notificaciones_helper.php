69efb6c21980c24a07a3889e1248d125066c565d
Angel <AngelVanegas944@gmail.com>
2025-09-27 22:57:43 -0500

    modificación helpers problemas de compatibilidad entre navegadores, alidación y envío de formularios

--- helpers/notificaciones_helper.php
+++ helpers/notificaciones_helper.php
@@ -3,20 +3,16 @@ function obtenerNotificacionesUsuario($usuarioId) {
     require_once __DIR__ . '/../models/Notificacion.php';
     require_once __DIR__ . '/../models/Intercambio.php';
     require_once __DIR__ . '/../models/Libro.php';
-
     $notificacionModel = new Notificacion();
     $notificacionesRaw = $notificacionModel->obtenerPorUsuario($usuarioId);
     $notificaciones = [];
     foreach ($notificacionesRaw as $n) {
-        // Asegura que los campos sean los que espera el layout y agrega id, leida, intercambio_id
-        // Si la notificación es de tipo "aceptada/rechazada" y el link apunta a misIntercambios, usar la nueva acción para marcar como leída
         $link = $n['link'] ?? '';
         if (strpos($link, 'a=misIntercambios') !== false) {
             $link = 'index.php?c=IntercambioController&a=marcarNotificacionYRedirigir';
         } elseif (empty($link)) {
             $link = 'index.php?c=IntercambioController&a=notificaciones';
         }
-        // Siempre agregar el parámetro noti=ID
         $sep = (strpos($link, '?') !== false) ? '&' : '?';
         $link_final = $link . $sep . 'noti=' . ($n['id'] ?? '');
         $notificaciones[] = [
@@ -28,6 +24,5 @@ function obtenerNotificacionesUsuario($usuarioId) {
             'intercambio_id' => $n['intercambio_id'] ?? null
         ];
     }
-
     return $notificaciones;
 }
