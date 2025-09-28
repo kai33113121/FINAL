8ca3b206b55cd656b9067fef3667ad2c7ae94f54
Angel <AngelVanegas944@gmail.com>
2025-09-27 21:50:31 -0500

    Sobreescritura de rutas principales, carga inicial de index

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 session_start();
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $c = $_GET['c'] ?? null;
 $a = $_GET['a'] ?? null;
-
 if ($c && $a) {
     $controllerFile = "controllers/{$c}.php";
-
     if (file_exists($controllerFile)) {
         require_once $controllerFile;
-
         if (class_exists($c)) {
             $controller = new $c();
-
             if (method_exists($controller, $a)) {
                 $controller->$a();
             } else {
