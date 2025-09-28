ec735b85b8462900596ad0d1a5a7b41f8d21d8a0
Angel <AngelVanegas944@gmail.com>
2025-09-27 22:46:57 -0500

    modificado el controlador de generos para implementación de la visual

--- controllers/GenerosController.php
+++ controllers/GenerosController.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/../models/Libro.php';
-
 class GenerosController
 {
     public function index()
@@ -9,16 +8,11 @@ class GenerosController
             header("Location: index.php?c=UsuarioController&a=login");
             exit;
         }
-
         $libroModel = new Libro();
-        
-        // Usar tu método existente obtenerTodos()
         $libros = $libroModel->obtenerTodos();
-
         require_once __DIR__ . '/../helpers/notificaciones_helper.php';
         $usuario_id = $_SESSION['usuario']['id'];
         $notificaciones = obtenerNotificacionesUsuario($usuario_id);
-
         $contenido = __DIR__ . '/../views/usuario/generos.php';
         include __DIR__ . '/../views/layouts/layout_usuario.php';
     }
